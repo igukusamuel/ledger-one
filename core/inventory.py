576fd7c81e6eb94f25965b88a894fcc17e63e363
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-15 02:46:03 -0500

    Update inventory.py

--- core/inventory.py
+++ core/inventory.py
@@ -1,17 +1,17 @@
-import pandas as pd
-
-# Demo inventory
-INVENTORY = pd.DataFrame([
-    {"Product": "Espresso", "Price": 3.00, "Stock": 100},
-    {"Product": "Latte", "Price": 4.50, "Stock": 100},
-    {"Product": "Cappuccino", "Price": 4.00, "Stock": 100},
-    {"Product": "Blueberry Muffin", "Price": 2.75, "Stock": 50},
-])
+from core.persistence import load_inventory, save_inventory
 
 
-def get_inventory():
-    return INVENTORY
+def update_inventory(sku, quantity_sold):
+    inventory = load_inventory()
 
+    if sku not in inventory:
+        raise Exception("SKU not found")
 
-def update_inventory(product, quantity):
-    INVENTORY.loc[INVENTORY["Product"] == product, "Stock"] -= quantity
+    inventory[sku]["quantity"] -= quantity_sold
+
+    if inventory[sku]["quantity"] < 0:
+        raise Exception("Negative inventory not allowed")
+
+    save_inventory(inventory)
+
+    return inventory[sku]["unit_cost"] * quantity_sold
