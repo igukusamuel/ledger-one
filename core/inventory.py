cbbb8dafa1b8e674d7aed53b6294dbd0222f26cd
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-15 02:51:22 -0500

    Update inventory.py

--- core/inventory.py
+++ core/inventory.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from core.persistence import load_inventory, save_inventory
 
 # Demo inventory
 INVENTORY = pd.DataFrame([
@@ -12,6 +13,32 @@ INVENTORY = pd.DataFrame([
 def get_inventory():
     return INVENTORY
 
-
 def update_inventory(product, quantity):
     INVENTORY.loc[INVENTORY["Product"] == product, "Stock"] -= quantity
+
+def update_inventory(sku, quantity_sold):
+    inventory = load_inventory()
+
+    if sku not in inventory:
+        raise Exception("SKU not found")
+
+    inventory[sku]["quantity"] -= quantity_sold
+
+    if inventory[sku]["quantity"] < 0:
+        raise Exception("Negative inventory not allowed")
+
+    save_inventory(inventory)
+
+    return inventory[sku]["unit_cost"] * quantity_sold
+
+
+def add_inventory_item(sku, name, quantity, unit_cost):
+    inventory = load_inventory()
+
+    inventory[sku] = {
+        "name": name,
+        "quantity": quantity,
+        "unit_cost": unit_cost
+    }
+
+    save_inventory(inventory)
