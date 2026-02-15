fbfc2bc12b4a3b3bba382b055abc1b57cd4c1dbc
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-14 22:02:02 -0500

    Update journals.py

--- core/journals.py
+++ core/journals.py
@@ -4,12 +4,18 @@ from datetime import date
 
 @dataclass
 class JournalEntry:
-    def __init__(self, entry_date, debit_account, credit_account, amount, description):
+
+    def __init__(self, entity_id, entry_date,
+                 debit_account, credit_account,
+                 amount, description):
+
+        self.entity_id = entity_id
         self.entry_date = entry_date
         self.debit_account = debit_account
         self.credit_account = credit_account
         self.amount = amount
         self.description = description
+
 
 def generate_accrual_journal(accrual_date: date, amount: float):
     return [
