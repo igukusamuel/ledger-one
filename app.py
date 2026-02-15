0aba5c6facdb0722d6e8223a5a92ee4dcc24cb1f
igukusamuel <32755662+igukusamuel@users.noreply.github.com>
2026-02-15 13:41:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,13 @@
 import streamlit as st
 
-from tabs.trade_capture import render as trade_capture_render
-from tabs.subledger import render as subledger_render
-from tabs.general_ledger import render as gl_render
-from tabs.tax import render as tax_render
-from tabs.cafe import render as cafe_render
-from core.persistence import initialize_db
-
-initialize_db()
-
 st.set_page_config(layout="wide")
+
 st.title("LedgerOne ERP System")
 
-tabs = st.tabs([
-    "Trade Capture",
-    "Subledger",
-    "General Ledger",
-    "Tax",
-    "Cafe"
-])
+st.write("APP LOADED")
 
-with tabs[0]:
-    trade_capture_render()
+import tabs.cafe as cafe
 
-with tabs[1]:
-    subledger_render()
+st.write("CAFE IMPORTED")
 
-with tabs[2]:
-    gl_render()
-
-with tabs[3]:
-    tax_render()
-
-with tabs[4]:
-    cafe_render()
+cafe.render()
